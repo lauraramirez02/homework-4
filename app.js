27a0956c8506de8cbbeb753c7359d369a6b9b583
Laura Ramirez <laura.ramirez02@student.csulb.edu>
2019-10-09 23:39:46 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var x= event.which || event.keyCode;
 
-
-
 event.keyCode
 
 document.getElementById("balloon").onKeyPress = function () {myFunction()};
-function myFunction () {
-    document.getElementById("balloon").style.fontSize= ""
+function uniKeyCode (event) {
+    var key = event.keyCode;
+    document.getElementById("balloonBi").style.fontSize= "10px"
 }

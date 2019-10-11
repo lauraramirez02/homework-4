c73ba7aec2cf31d29eb60e24a5da93c751424869
Laura Ramirez <laura.ramirez02@student.csulb.edu>
2019-10-10 18:58:25 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
 document.body.innerText= String.fromCodePoint (0X1F388);
-
-document.getElementById("balloon").onKeyPress = function () {myFunction()};
-function uniKeyCode (event) {
-    var key = event.keyCode;
-    document.getElementById("balloonBi").style.fontSize= "40px"
-}

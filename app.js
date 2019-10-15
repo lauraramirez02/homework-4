5b62b0967aaac1a7a6b663d19076d2a591c9c2a5
Laura Ramirez <laura.ramirez02@student.csulb.edu>
2019-10-14 17:38:50 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-const balloon= document.getElementById('balloon')
+//const balloon= document.getElementById('balloon')
 
-const keyboardInfo = e=> {
-    console.log('Keyboard event: ${e.type}, key: ${e.key}, code: ${e.code');
-};
-document.addEventListener("keypress", keyboardInfo);
+//const keyboardInfo = e=> {
+//    console.log('Keyboard event: ${e.type}, key: ${e.key}, code: ${e.code');
+//};
+//document.addEventListener("keypress", key);
 
-//eventTarget.addEventListener("keypress", event => {
-//    if (event.)
+//window.addEventListener("keypress", event => {
+//    if (event.key == "i") {
+//    document.balloon.style.width = "10px";
+//}
 //});
+
+var balloon;
+var maxBlows;
+var currentBlow= 0;
+var boom;
+
+function blow () {
+    currentBlow = currentBlow + 1;
+    if (currentBlow <= maxBlows) {
+        var blowAmount = 50;
+        var balloonWidth = parseInt (balloon.style.width);
+        var newBalloonWidth = blowAmount + balloonWidth;
+        balloon.style.width = newBalloonWidth + "px";
+        
+        count.innerHTML = currentBlow;
+    } else {
+        balloon.style.display ='none';
+    boom.style.display = "block";
+}
+    
+}
+
+

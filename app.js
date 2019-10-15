6cd61042312835e78aa3f54393fb564db723250d
Laura Ramirez <laura.ramirez02@student.csulb.edu>
2019-10-15 13:46:40 -0700

    Update app.js

--- app.js
+++ app.js
@@ -10,59 +10,35 @@
 //    document.balloon.style.width = "10px";
 //}
 //});
-function getSizeValue(valueWithUnit, unit) {
-	return parseInt(valueWithUnit.split(unit)[0]);
-}
+var init=16;
+window.addEventListener("keyup",arrow);
 
-function balloon(event) {
-	// if (event.defaultPrevented) {
-	//   return; // Do nothing if the event was already processed
-	// }
-	switch (event.keyCode) {
-		case 40:
-			changeSize('.balloon', 1.1, 'down');
-			break;
-		case 38:
-			changeSize('.balloon', 1.1);
-			break;
-		default:
-			return; // Quit when this doesn't handle the key event.
-	}
+function arrow(event) {
+    event.preventDefault ();
+    var balloon = document.getElementById("balloon");
+    switch (event.keyCode) {
+        case 73:
+            init=init-2;
+            balloon.style.fontSize-init+"px";
+            if (init <=0) {
+                balloon.textContent="Done";
+                balloon.style.fontSize="60px";
+                window.removeEventListener('keyup',minus);
+            }
+                
+            break;
+            case 68:
+            if (init >=60) {
+                balloon.textContent="💥";
+                window.removeEventListener('keyup',plus);
+            }
+    
+            else{
+                init-init+2;
+                balloon.style.fontSize=init+"px";
+                )break;
+            }
+            }
+    }
 
-	// Cancel the default action to avoid it being handled twice
-	// event.preventDefault();
-}
-
-function question() {
-  $( ".exercise" ).toggleClass( "active" );
-}
-
-// element = 'string'
-// amount = int
-// direction '' or down
-function changeSize(element, amount, direction) {
-	element = document.querySelector(element);
-	let elementSize = window.getComputedStyle(element).fontSize;
-	let elementSizeValue = getSizeValue(elementSize, 'px');
-
-	if (direction === undefined) {
-		elementSizeValue *= amount;
-	} else elementSizeValue /= amount;
-
-	if (elementSizeValue <= 250) {
-		console.log(elementSize);
-		elementSize = Math.ceil(elementSizeValue) + 'px';
-		element.style.fontSize = elementSize;
-	} else boom(element);
-}
-
-function boom(element) {
-	element.firstChild.nodeValue = '💥';
-	document.removeEventListener('keydown', balloon, true);
-}
-
-document.addEventListener('keydown', balloon, true);
-
-button = document.querySelector('button.question');
-button.addEventListener('click', question, true);
 

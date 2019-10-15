5304b83920c40a689d21637f820f5a7c73498788
Laura Ramirez <laura.ramirez02@student.csulb.edu>
2019-10-15 15:25:03 -0700

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function arrow(event) {
             balloon.style.fontSize=init+"px";
             if (init <=0) {
                 balloon.textContent="done";
-                balloon.style.fontSize="10px";
+                balloon.style.fontSize="60px";
                 window.removeEventListener('keyup',minus);
             }
                 

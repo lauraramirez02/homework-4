5cd5530ed1aec6cab8ab951ed2b21a86f3e996cb
Laura Ramirez <laura.ramirez02@student.csulb.edu>
2019-10-15 15:19:04 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-//const balloon= document.getElementById('balloon')
-
-//const keyboardInfo = e=> {
-//    console.log('Keyboard event: ${e.type}, key: ${e.key}, code: ${e.code');
-//};
-//document.addEventListener("keypress", key);
-
-//window.addEventListener("keypress", event => {
-//    if (event.key == "i") {
-//    document.balloon.style.width = "10px";
-//}
-//});
 var init=16;
 window.addEventListener("keyup",arrow);
 

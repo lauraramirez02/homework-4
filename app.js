43f5d743cba625c8efd17028ed94561910986b6b
Laura Ramirez <laura.ramirez02@student.csulb.edu>
2019-10-15 19:46:32 -0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function arrow(event) {
     switch (event.keyCode) {
         case 68:
             init=init-2;
-            balloon.style.fontSize=init-"px";
+            balloon.style.fontSize=init+"px";
             if (init <=0) {
                 balloon.textContent="done";
                 balloon.style.fontSize="60px";
